be7d340667d474d846edd13571187e5b2d638bbe
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-22 14:06:09 -0300

    Create DicionarioExemplo2.py

---
+++ DicionarioExemplo2.py
@@ -0,0 +1,13 @@
+print('-='*23)
+print('{:^46}'.format(' EXEMPLO DE DICIONÁRIO 2'))
+print('-='*23)
+estado = dict()
+brasil = list()
+for c in range(0,3):
+    estado['UF'] = str(input('Unidade Federativa: '))
+    estado['UF'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()     
