46c8ec586932372b61bffe7d4ad571d7a60fd18e
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-24 20:33:51 -0300

    JOGO DE DADOS
    COM NOVAS FUNCIONALIDADES

---
+++ JOGOdeDADOS.py
@@ -0,0 +1,23 @@
+print('-='*23)
+print('{:^46}'.format(' JOGO DE DADOS - OBSERVAÇÃO -> COM NOVAS FUNÇOES '))
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'JOGADOR MARINO ': randint(1,6),
+        'JOGADOR EDUARDO ': randint(1,6), 
+        'JOGADOR AMANDA ': randint(1,6), 
+        'JOGADOR JOANA ': randint(1,6)}
+print(' ')
+ranking = list()
+print('-='*7,' VALORES SORTEADOS ','-='*7)
+for jogador, dados in jogo.items():
+    sleep(1)
+    print(f'O {jogador}, Jogou O Dado e tirou  no {dados} dado.')
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+
+print(' ')  
+print('-='*7, ' RANKING DOS JOGADORES ','-='*7)
+for cont, vencedor in enumerate(ranking):
+    sleep(1)
+    print(f'{cont+1}º LUGAR -> O {vencedor[0]} com {vencedor[1]}')
+print('-='*23)
